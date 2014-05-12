525b3acaef6457a93b906f8202497aa5f73e2214
tentenmitsunori <tentemitsunori@gmail.com>
2014-05-12 22:49:54 +0900

    update

--- tasklist/gen/com/example/tasklist/R.java
+++ tasklist/gen/com/example/tasklist/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f050047;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1105,13 +1105,16 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int foodTaskListView=0x7f050044;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int lifeStyleTaskListView=0x7f050045;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int listwrapper=0x7f05003e;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1120,6 +1123,11 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int radio0=0x7f050040;
+        public static final int radio1=0x7f050041;
+        public static final int radio2=0x7f050042;
+        public static final int radioGroup1=0x7f05003f;
+        public static final int scrollView1=0x7f050043;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1140,6 +1148,7 @@ containing a value of this type.
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int trainingTaskListView=0x7f050046;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1197,6 +1206,7 @@ containing a value of this type.
         public static final int activity_main=0x7f030018;
         public static final int fragment_main=0x7f030019;
         public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int tasklist_fragment=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- tasklist/src/com/example/tasklist/MainActivity.java
+++ tasklist/src/com/example/tasklist/MainActivity.java
@@ -125,7 +125,14 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch(position){
+            	case 0:
+            		return new TaskFragment();
+            	case 1:
+            		return PlaceholderFragment.newInstance(position + 1);
+            	default :
+            		return PlaceholderFragment.newInstance(position + 1);
+            }        	
         }
 
         @Override

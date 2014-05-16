6d1ef1d06dc0608910a3b3df0150ff1d04e94e94
tentenmitsunori <tentemitsunori@gmail.com>
2014-05-17 03:31:06 +0900

    tasklist

--- tasklist/src/com/example/tasklist/PostFragment.java
+++ tasklist/src/com/example/tasklist/PostFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 public class PostFragment extends Fragment {
 	@Override	
@@ -12,6 +13,39 @@ public class PostFragment extends Fragment {
 			Bundle savedInstanceState) {
 		// このフラグメント用のレイアウトをインフレートする
 		View v = inflater.inflate(R.layout.post_fragment, container, false);
+	    setRadioGroupChecked(v);
 		return v;
+	}
+	
+	public void setRadioGroupChecked(View v){
+		final View view = v;
+	    RadioGroup group = (RadioGroup)view.findViewById(R.id.radioGroup2);
+	    group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+	    	@Override
+	    	public void onCheckedChanged(RadioGroup group, int checkedId) {
+	    		View ftp = (View)view.findViewById(R.id.foodTaskPostLayout);
+	    		View ltp = (View)view.findViewById(R.id.lifeStyleTaskPostLayout);
+	    		View ttp = (View)view.findViewById(R.id.trainingTaskPostLayout);
+	    		switch(checkedId){
+	    			case R.id.radioFood2:
+	    				ftp.setVisibility(View.VISIBLE);
+	    				ltp.setVisibility(View.GONE);
+	    				ttp.setVisibility(View.GONE);
+	    				return;
+	    			case R.id.radioLifeStyle2:
+	    				ftp.setVisibility(View.GONE);
+	    				ltp.setVisibility(View.VISIBLE);
+	    				ttp.setVisibility(View.GONE);
+	    				return;
+	    			case R.id.radioTraining2:
+	    				ftp.setVisibility(View.GONE);
+	    				ltp.setVisibility(View.GONE);
+	    				ttp.setVisibility(View.VISIBLE);
+	    				return;
+	    			default:
+	    				return;
+	    		}
+	    	}
+	    });	
 	}
 }
